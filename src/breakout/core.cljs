b92285039b57fd12ec6ea4f8a53f908f10a470cb
Jarkko Saltiola <jarkko.saltiola@gmail.com>
2015-05-29 19:48:22 +0300

    Initial commit.

---
+++ src/breakout/core.cljs
@@ -0,0 +1,21 @@
+(ns ^:figwheel-always breakout.core
+    (:require
+     [quil.core :as q :include-macros true]
+     [quil.middleware :as m]
+     ))
+
+(enable-console-print!)
+
+(println "Edits to this text should show up in your developer console.")
+
+;; define your app data so that it doesn't get over-written on reload
+
+(defonce app-state (atom {:text "Hello world!"}))
+
+
+(defn on-js-reload []
+  ;; optionally touch your app-state to force rerendering depending on
+  ;; your application
+  ;; (swap! app-state update-in [:__figwheel_counter] inc)
+) 
+

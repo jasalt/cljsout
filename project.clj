3799c45821c9f97b325b668d7d8bd8e7667ffb9a
Jarkko Saltiola <jarkko.saltiola@gmail.com>
2015-06-28 23:55:28 +0300

    Add Quil template.

--- project.clj
+++ project.clj
@@ -5,10 +5,10 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-3211"]
-                 [quil "2.2.5" :exclusions [org.clojure/clojure]]
+                 [quil "2.2.6" :exclusions [org.clojure/clojure]]
                  ]
   :plugins [[lein-cljsbuild "1.0.5"]
-            [lein-figwheel "0.3.3"]
+            [lein-figwheel "0.3.5"]
             ]
   :source-paths ["src"]
   :clean-targets ^{:protect false} ["resources/public/js/compiled" "target"]

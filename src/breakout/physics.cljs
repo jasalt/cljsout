a4cc4a0364b31273eb96d9d8383a69bf2dd901b1
Jarkko Saltiola <jarkko.saltiola@gmail.com>
2015-07-18 21:02:06 +0300

    Identify collision with brick.

--- src/breakout/physics.cljs
+++ src/breakout/physics.cljs
@@ -54,26 +54,6 @@
     (swap! ball assoc :angle (- angle))))
 
 
-(defn collides-side? [obj obj2]
-  "Check if ball collides given rect defined by top-left and bottom-right.
-   Return collision position keyword (:right :top :bottom :left) or nil."
-  ;;ball-x ball-y tl-x tl-y br-x br-y
-  (let [br (geom/bottom-right obj)
-        tl (geom/top-left obj)
-        br2 (geom/bottom-right obj2)
-        tl2 (geom/top-left obj2)]
-    (and 
-     ;;If the tops are higher than the bottoms
-     (and (< (:y tl) (:y br2))
-          (< (:y tl2) (:y br)))
-     ;;And the lefts are "lefter" than the rights
-     (and (< (:x tl) (:x br2))
-          (< (:x tl2) (:x br)))))
-  
-  ;;TODO check collision side
-  ;; horizontal or if x is far from center
-  )
-
 (defn check-ball-collisions [monet-canvas ball pad]
   "Check if ball collides somewhere and change it's angle accordingly."
   (let [ball-x (@ball :x)
@@ -95,10 +75,12 @@
       :else
       (let [game-entities (breakout.game/game-canvas :entities)
             brick-keys (descendants ::breakout.game/brick)
-            ;; Access as js property for performance.
-            brick-entities (map #(aget game-entities (str %)) brick-keys)
+            ;; Access as js property for performance. TODO find a better way?
+            bricks (map #(vector % ((aget game-entities (str %)) :value))
+                                brick-keys)
+            colliding-bricks (filter #(geom/collision? @ball (second %)) bricks)
             ]
-        brick-entities
+        (when-not (empty? colliding-bricks) (print colliding-bricks))
         )
       ;; map all bricks
       ;; check collision and calculate nearest side to decide.

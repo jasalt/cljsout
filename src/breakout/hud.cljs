18bdd212c7b6a6e3f8dce2805f75aba40564f035
Jarkko Saltiola <jarkko.saltiola@gmail.com>
2015-07-21 18:42:53 +0300

    Some tweaks.

--- src/breakout/hud.cljs
+++ src/breakout/hud.cljs
@@ -1,17 +1,28 @@
 ;; Debug some values in DOM.
 (ns breakout.hud
-  (:require [reagent.core :as r])
+  (:require
+   [reagi.core :as r]
+   [reagent.core :as dom]
+   [breakout.input :as input]
+   )
   )
 
 (defonce hud-state
-  (r/atom
+  (dom/atom
    {:accelerometer "[Waiting for accelerometer values]"}))
+
+;; UI atom gets updated by stream that is thorttled.
+(def ui-update-stream
+  (->> input/mouse-position-stream
+       (r/map print)
+   )
+  )
+
 
 (defn accelerometer-view []
   [:p (str (:accelerometer @hud-state))
    ])
 
-(r/render-component
+(dom/render-component
  [accelerometer-view]
  (.getElementById js/document "hud"))
-
--- src/breakout/input.cljs
+++ src/breakout/input.cljs
@@ -5,14 +5,12 @@
    [monet.canvas :as canvas]
    [breakout.game :refer [game-canvas canvas-dom ball pad]]
    [breakout.utils :refer [log]]
-   [breakout.physics :refer [move-ball! move-left! move-right! move-to!]]
+   [goog.events :as g-events]
+   ;;[breakout.physics :refer [move-ball! move-left! move-right! move-to!]]
    [breakout.utils :refer [scale-value str-float]]
-   [breakout.hud :refer [hud-state]]
+   ;;[breakout.hud :refer [hud-state]]
    ))
 
-;; Canvas x-offset will be subtracted from read mouse x values.
-(def mouse-x-offset
-  (.ceil js/Math (.-left (.getBoundingClientRect canvas-dom))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Setup controls
@@ -25,84 +23,99 @@
 
 ;;;; Separate streams for keydowns and keyups
 
-(defn keydown-stream []
-  (let [out (r/events)]
-    (set! (.-onkeydown js/document)
-          #(r/deliver out [::down (.-keyCode %)]))
-    out))
+;; (defn keydown-stream []
+;;   (let [out (r/events)]
+;;     (set! (.-onkeydown js/document)
+;;           #(r/deliver out [::down (.-keyCode %)]))
+;;     out))
 
-(defn keyup-stream []
-  (let [out (r/events)]
-    (set! (.-onkeyup js/document)
-          #(r/deliver out [::up (.-keyCode %)]))
-    out))
+;; (defn keyup-stream []
+;;   (let [out (r/events)]
+;;     (set! (.-onkeyup js/document)
+;;           #(r/deliver out [::up (.-keyCode %)]))
+;;     out))
 
 ;; Merge key events into single event stream that
 ;; reduces active keys into set and gets updated every 25 ms.
 
-(def active-keys-stream
-  (->> (r/merge (keydown-stream) (keyup-stream))
-       (r/reduce (fn [acc [event-type key-code]]
-                   (condp = event-type
-                     ::down (conj acc key-code)
-                     ::up (disj acc key-code)
-                     acc))
-                 #{})
-       (r/sample 25)))
+;; (def active-keys-stream
+;;   (->> (r/merge (keydown-stream) (keyup-stream))
+;;        (r/reduce (fn [acc [event-type key-code]]
+;;                    (condp = event-type
+;;                      ::down (conj acc key-code)
+;;                      ::up (disj acc key-code)
+;;                      acc))
+;;                  #{})
+;;        (r/sample 25)))
 
-(defn filter-map [pred f & args]
-  "Helper function for responding to active keys with actions.
-   Takes key type predicate and function with args thats evaluated.
-   Eg: (filter-map #{RIGHT} move-right! pad)"
-  (->> active-keys-stream
-       (r/filter (partial some pred))
-       (r/map (fn [_] (apply f args)))))
+;; (defn filter-map [pred f & args]
+;;   "Helper function for responding to active keys with actions.
+;;    Takes key type predicate and function with args thats evaluated.
+;;    Eg: (filter-map #{RIGHT} move-right! pad)"
+;;   (->> active-keys-stream
+;;        (r/filter (partial some pred))
+;;        (r/map (fn [_] (apply f args)))))
 
 (defn pause! []
   (if @(:updating? game-canvas)
     (canvas/stop-updating game-canvas)
     (canvas/start-updating game-canvas)))
 
-(->> active-keys-stream
-     (r/filter (partial some #{PAUSE}))
-     (r/throttle 100) ;; simple debounce
-     (r/map pause!))
+;; (->> active-keys-stream
+;;      (r/filter (partial some #{PAUSE}))
+;;      (r/throttle 100) ;; simple debounce
+;;      (r/map pause!))
 
-(filter-map #{RIGHT} move-right! pad)
-(filter-map #{LEFT} move-left! pad)
-(filter-map #{SPACE} move-ball! ball)
+;;(filter-map #{RIGHT} move-right! pad)
+;;(filter-map #{LEFT} move-left! pad)
+;;(filter-map #{SPACE} move-ball! ball)
 
 ;;;; Pad control with mouse and orientation
 
-(defn mouse-move-stream []
-  (let [out (r/events 100)
-        game-canvas (.getElementById js/document "game")]
-    (set! (.-onmousemove game-canvas)
-          #(r/deliver out (.-clientX %)))
-    out))
+(defonce mouse-move-stream (r/events 100))
+
+(defn start-mouse-listener []
+ (let [game-canvas (.getElementById js/document "game")]
+   (set! (.-onmousemove game-canvas) #(r/deliver mouse-move-stream (.-clientX %)))))
+
+(defn stop-mouse-listener []
+  (let [game-canvas (.getElementById js/document "game")]
+    ;;(.removeEventListener game-canvas put-to-mouse-stream)
+    (set! (.-onmousemove game-canvas) nil)))
+
+(start-mouse-listener)
 
 (def mouse-position-stream
-  (->> (mouse-move-stream)
+  (->> mouse-move-stream
        (r/uniq) ;; Drop duplicate events
-       (r/map #(move-to! pad (- % mouse-x-offset)))
+       
        ))
+
+(defonce orientation-source (r/events))
+(defn set-orientation-listener []
+  (.addEventListener js/window "deviceorientation"
+                     #(r/deliver orientation-source
+                                 {:alpha (.-alpha %)
+                                  ;;:beta  (.-beta %)
+                                  :gamma (.-gamma %)}) false))
+(set-orientation-listener)
 
 (defn orientation-stream []
-  (let [out (r/events)]
-    (.addEventListener js/window "deviceorientation"
-                       #(r/deliver out {:alpha (.-alpha %)
-                                        ;;:beta  (.-beta %)
-                                        :gamma (.-gamma %)}) false)
-    out))
+  ;; Normalize orientation stream values
+  (let [chan (r/events)]
+    (->> ;;(r/subscribe chan orientation-source)
+     orientation-source
+     (r/uniq) ;; Drop duplicate events
+     (r/map #(do
+               (let [gamma (:gamma %)
+                     scaled-val (scale-value gamma [-30 30] [0 300])]
+                 scaled-val
 
-(defonce orientations-stream
-  (->> (orientation-stream)
-       (r/uniq) ;; Drop duplicate events
-       (r/map #(do
-                 (let [gamma (:gamma %)
-                       scaled-val (scale-value gamma [-30 30] [0 300])]
-                   (swap! hud-state assoc :accelerometer
-                          (map str-float [gamma scaled-val]))
-                   (move-to! pad scaled-val))
-                 ))
-       ))
+                 ;; (move-to! pad scaled-val)
+                 )
+               ))
+     )
+    chan
+    ))
+
+

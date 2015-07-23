e62fcdcb778f4901c148ae69b34ba6535021cb7c
Jarkko Saltiola <jarkko.saltiola@gmail.com>
2015-07-23 09:05:13 +0300

    Initialize game after window onload event.

--- src/breakout/core.cljs
+++ src/breakout/core.cljs
@@ -5,33 +5,34 @@
      [monet.canvas :as canvas]
      [reagi.core :as r]
 
+     ;;TODO cleanup and order properly
+     [breakout.input]
      [breakout.game :refer [game-canvas ball-entity pad-entity]]
      [breakout.utils :refer [log]]
      [breakout.entities :as entities]
-     
-     ;;[breakout.input]
-     
-     
      ))
 
 (enable-console-print!) ;; Route prints to console
 
-(canvas/add-entity game-canvas :ball-entity ball-entity)
-(canvas/add-entity game-canvas :pad-entity pad-entity)
-(canvas/draw-loop game-canvas)
+(defn init! []
+  (canvas/add-entity game-canvas :ball-entity ball-entity)
+  (canvas/add-entity game-canvas :pad-entity pad-entity)
+  (canvas/draw-loop game-canvas)
+  )
 
-;; Game is stopped when starting up to avoid unnecessary calculation.
-;;(canvas/stop-updating game-canvas)
 
 (defn pause! []
   (if @(:updating? game-canvas)
     (canvas/stop-updating game-canvas)
     (canvas/start-updating game-canvas)))
 
+;;(pause!)
+
 (defn on-js-reload []
   ;; optionally touch your game-state to force rerendering depending on
   ;; your application
   ;; (swap! game-state update-in [:__figwheel_counter] inc)
+
   )
 
-
+(set! (.-onload js/window) init!)

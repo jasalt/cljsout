68fe24a8b0d3db546c33b5c3c32b619f4256d5b6
Jarkko Saltiola <jarkko.saltiola@koodilehto.fi>
2015-11-03 17:30:43 +0200

    Fix repl.

--- project.clj
+++ project.clj
@@ -4,15 +4,18 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.107"]
+                 [org.clojure/clojurescript "1.7.145"]
                  [com.cemerick/clojurescript.test "0.3.3"]
                  [reagi "0.10.1"         ;; FRP library based on core.async
                   :exclusions [org.clojure/clojure]]
-                 [rm-hull/monet "0.2.1"] ;; Canvas library
-                 [reagent "0.5.0"]       ;; React.js
+                 [rm-hull/monet "0.2.2"] ;; Canvas library
+                 [reagent "0.5.1"]       ;; React.js
+                 [com.cemerick/piggieback "0.2.1"]
+                 [org.clojure/tools.nrepl "0.2.10"]
                  ]
+  :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
   :plugins [[lein-cljsbuild "1.0.5"]
-            [lein-figwheel "0.3.5"]]
+            [lein-figwheel "0.4.1"]]
   :source-paths ["src"]
   :clean-targets ^{:protect false} ["resources/public/js/compiled" "target"]
   :cljsbuild {
@@ -32,30 +35,11 @@
                          :pretty-print false}}]}
 
   :figwheel {:http-server-root "public" ;; default and assumes "resources" 
-             ;; :server-port 3449 ;; default
+             :server-port 3449 ;; default
              :css-dirs ["resources/public/css"] ;; watch and update CSS
-
-             ;; Start an nREPL server into the running figwheel process
+             ;;:repl true
              :nrepl-port 7888
-
-             ;; Server Ring Handler (optional)
-             ;; if you want to embed a ring handler into the figwheel http-kit
-             ;; server, this is for simple ring servers, if this
-             ;; doesn't work for you just run your own server :)
-             ;; :ring-handler hello_world.server/handler
-
-             ;; To be able to open files in your editor from the heads up display
-             ;; you will need to put a script on your path.
-             ;; that script will have to take a file path and a line number
-             ;; ie. in  ~/bin/myfile-opener
-             ;; #! /bin/sh
-             ;; emacsclient -n +$2 $1
-             ;;
-             :open-file-command "myfile-opener"
-
-             ;; if you want to disable the REPL
-             ;; :repl false
-
+             :open-file-command "ec"
              ;; to configure a different figwheel logfile path
              ;; :server-logfile "tmp/logs/figwheel-logfile.log" 
              })
